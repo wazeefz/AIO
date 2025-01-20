9f661645a7b11930ae0c2ca4ca0aa600e672099a
Sara <sarasoraya.c137@gmail.com>
2025-01-20 14:47:00 +0800

    test

---
+++ src/pages/sara/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <!-- <HelloWorld /> -->
+  <div class="bg-primaryBrown ma-16">
+    <p>Hello semuaaa</p>
+  </div>
+
+  <div class="bg-primaryGrey ma-16">
+    <p>Hello test</p>
+  </div>
+  <div class="bg-secondaryGrey ma-16">
+    <p>Hello test</p>
+  </div>
+</template>
+
+<script setup>
+//
+</script>
