dc11745cd01bb4ce9ed91c6775b79484e7690824
Sara <sarasoraya.c137@gmail.com>
2025-01-20 14:01:39 +0800

    hiiii
    apa222

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <!-- <HelloWorld /> -->
   <div class="bg-primaryBrown ma-16">
-    <p>Hello test</p>
+    <p>Hello semuaaa</p>
   </div>
   <div class="bg-secondaryBrown ma-16">
-    <p>Hello test</p>
+    <p>Hello again</p>
   </div>
   <div class="bg-primaryGrey ma-16">
     <p>Hello test</p>
