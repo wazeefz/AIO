ee004d1476116c76bde94443b9b7a5507d65b82d
Sara <sarasoraya.c137@gmail.com>
2025-01-21 14:49:04 +0800

    new table with vuetify

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import vuetify from './plugins/vuetify'
+
+//CSS
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
 
 // Components
 import App from './App.vue'
@@ -16,5 +21,7 @@ import { createApp } from 'vue'
 const app = createApp(App)
 
 registerPlugins(app)
+
+app.use(vuetify)
 
 app.mount('#app')
