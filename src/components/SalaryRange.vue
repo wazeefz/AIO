5919bc735a2c3d976e08b6486736c5b6a585f9d1
maro <omaraziz908@gmail.com>
2025-01-22 13:01:31 +0800

    mine

---
+++ src/components/SalaryRange.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row>
+    <v-col cols="6">
+      <v-text-field
+        v-model="minSalary"
+        label="RM"
+        type="number"
+        min="0"
+        hide-details
+        density="compact"
+        class="salary-input"
+      ></v-text-field>
+    </v-col>
+    <v-col cols="6">
+      <v-text-field
+        v-model="maxSalary"
+        label="RM"
+        type="number"
+        min="0"
+        hide-details
+        density="compact"
+        class="salary-input"
+      ></v-text-field>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue'
+
+const minSalary = ref(6000)
+const maxSalary = ref(20000)
+
+const emit = defineEmits(['update:range'])
+
+watch([minSalary, maxSalary], ([newMin, newMax]) => {
+  emit('update:range', { min: newMin, max: newMax })
+})
+</script>
---
+++ src/pages/Filter.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <FilterCard />
+    {/* Other content */}
+  </v-container>
+</template>
+
+<script>
+import FilterCard from '@/components/FilterCard.vue'
+
+export default {
+  components: {
+    FilterCard,
+  },
+}
+</script>
