7d4b35d09042553c886dfdf0d4be1e18e95d8ef1
Sara <sarasoraya.c137@gmail.com>
2025-02-07 10:28:56 +0800

    TeamMemberList
    change star color

--- src/components/Star.vue
+++ src/components/Star.vue
@@ -21,8 +21,8 @@ export default {
 .star {
   width: 50px;
   height: 50px;
-  background-color: #ccc;
-  clip-path: polygon(50% -2%, 100% 50%, 50% 100%, 0% 50%);
+  background-color: #000000;
+  clip-path: polygon(50% 0%, 100% 50%, 50% 100%, 0% 50%);
   transition: all 0.3s ease;
   cursor: pointer;
 }
