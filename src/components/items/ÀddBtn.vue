b8db0884ca2608f13488ae881400262b625feaba
maro <omaraziz908@gmail.com>
2025-02-13 14:20:53 +0800

    breakdown items
    I breakdown our component into items first

---
+++ src/components/items/ÀddBtn.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-btn v-if="isEditing" icon :color="color" @click="handleClick">
+    <v-icon>{{ icon }}</v-icon>
+  </v-btn>
+</template>
+
+<script setup>
+const props = defineProps({
+  isEditing: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: String,
+    default: 'green',
+  },
+  icon: {
+    type: String,
+    default: 'mdi-plus',
+  },
+})
+
+const emit = defineEmits(['click'])
+
+const handleClick = () => {
+  emit('click')
+}
+</script>
