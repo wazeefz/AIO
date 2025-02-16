436b9e41cb9bbac5f81c92378b4c7327009958e9
maro <omaraziz908@gmail.com>
2025-02-16 16:39:16 +0800

    Prompt Page Default

--- src/pages/testing/prompt.vue
+++ src/pages/testing/prompt.vue
@@ -18,8 +18,37 @@
         ref="messagesContainer"
       >
         <transition name="fade" mode="out-in">
+          <!-- This is the prompt card by default -->
           <div v-if="currentChat.messages.length === 0" key="default">
-            <PromptPageDefault />
+            <!-- <PromptPageDefault /> component has been removed and included down here -->
+            <div
+              class="d-flex flex-column align-center justify-center fill-height"
+            >
+              <div class="d-flex flex-column align-center justify-center mb-10">
+                <p class="text-h3 font-weight-medium">All in One</p>
+                <p class="font-weight-regular">How can we help you today?</p>
+              </div>
+              <div class="d-flex flex-row align-center justify-center ga-2">
+                <div
+                  v-for="(item, index) in cards"
+                  :key="index"
+                  class="d-flex justify-center align-center flex-column w-30"
+                >
+                  <p
+                    class="text-secondaryBlack font-weight-medium text-h6 mb-4"
+                  >
+                    {{ item.title }}
+                  </p>
+                  <v-card
+                    :text="item.text"
+                    rounded="xl"
+                    class="text-white bg-secondaryBrown d-flex align-center text-center"
+                    max-width="300"
+                    height="100"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
           <div v-else key="message">
             <ChatMessage
@@ -36,13 +65,37 @@
   </div>
 </template>
 
+<script>
+// the text for prompt card
+export default {
+  data() {
+    return {
+      cards: [
+        {
+          title: 'Examples',
+          text: '"Help me assemble a team for a React native project"',
+        },
+        {
+          title: 'Capabilities',
+          text: 'Recommend the most optimized team for your projects including your internal and external resources',
+        },
+        {
+          title: 'Limitations',
+          text: 'May miss over other key talents due to unclear input',
+        },
+      ],
+    }
+  },
+}
+</script>
+
 <script setup>
 import { onMounted, ref, watch } from 'vue'
 import { useChat } from '@/composables/useChat'
 import ChatSidebar from '@/components//items/ChatSidebar.vue'
 import ChatMessage from '@/components/items/ChatMessage.vue'
 import ChatInput from '@/components/items/ChatInput.vue'
-import PromptPageDefault from '@/components/PromptPageDefault.vue'
+// import PromptPageDefault from '@/components/PromptPageDefault.vue'
 
 const {
   chatHistory,
